43c0d054a31ec7ce67f2cc79226b2b61bf4b19a5
Gregtmlg <greg.glemot@gmail.com>
2023-07-05 15:51:02 +0200

    Update training

--- traveling_salesman_training.py
+++ traveling_salesman_training.py
@@ -12,26 +12,29 @@ from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckA
 from traveling_salesman_env import TravelingSalesmanEnv
 
 # Set the parameters for the implementation
-max_timesteps = 2000000  # Maximum number of steps to perform
+MAX_TIMESTEPS = 10000000  # Maximum number of steps to perform
+SAVE_TIMESTEPS = 1000000 # save model every SAVE_TIMESTEPS step
 models_dir = "models/PPO_battery"
 logs_dir = "logs" 
 
-action_k = [0.1, 0.5, 1, 1.5, 2, 2.5, 3]
-step_k = [3, 4, 5]
+action_k = [0, 0.5, 1, 1.5, 2, 2.5, 3]
+step_k = [0, 0.5, 1, 1.5, 2]
 
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 
 if not os.path.exists(logs_dir):
     os.makedirs(logs_dir)
-ak = 0.5
-sk = 2
-env = TravelingSalesmanEnv(max_steps=500, type_of_use="simple", nb_goals=5, action_k=ak, step_k=sk)
-model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logs_dir, device="auto")
-# model.set_parameters(f"{models_dir}/traveling_salesman.zip")
-model.learn(total_timesteps=max_timesteps, reset_num_timesteps=False, tb_log_name=f"PPO_ak_{str(ak).translate(str.maketrans('', '', '.'))}_sk_{sk}_battery")
-model.save(f"{models_dir}/traveling_salesman_ak_{str(ak).translate(str.maketrans('', '', '.'))}_sk_{sk}_battery")
-env.close()
+for ak in action_k:
+    for sk in step_k:
+        env = TravelingSalesmanEnv(max_steps=250, type_of_use="simple", nb_goals=5, action_k=ak, step_k=sk)
+        model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logs_dir, device="auto")
+        iteration = 0
+        while SAVE_TIMESTEPS * iteration < MAX_TIMESTEPS:
+            iteration += 1
+            model.learn(total_timesteps=SAVE_TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO_ak_{str(ak).translate(str.maketrans('', '', '.'))}_sk_{sk}")
+            model.save(f"{models_dir}/travel_sales_{SAVE_TIMESTEPS * iteration}_ak_{str(ak).translate(str.maketrans('', '', '.'))}_sk_{sk}")
+        env.close()
 
 
 # plt.figure()
